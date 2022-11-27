67633285800f397fc4505cf508ffd6c3fcaf710d
AJ Shepherd <www.ajjps.com@hotmail.com>
2022-11-27 16:20:00 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 /*
-	Parallelism by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+Designed by Andrew J.Shepherd | plutoneon.com
 */
 
 var parallelism = (function($) { var _ = {
